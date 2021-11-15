8c524390fa8f667dfd04707c36f0e605b699cd66
Lubov <2l-u-b-o-v@ukr.net>
2021-11-15 19:08:30 +0200

    Реализует поворот карточек на секции "Products"

--- src/js/product-cards.js
+++ src/js/product-cards.js
@@ -7,8 +7,7 @@ document.getElementById("card_brown").addEventListener("click", Rotate);
 
     function Rotate(e) {
     this.parentNode.classList.toggle('is-reversed');
-    var element1 = document.getElementsByClassName("products-list__description_2");
-    element1.classList.remove("visually__hidden");
+    
     }
 
 })();
