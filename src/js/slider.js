f2b4fa2faa3ad39daff8c124d3f1b7546fdb2463
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2021-11-15 19:34:42 +0200

    add slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,8 @@
 new Swiper('.reviews__slider', {
-   // navigator: {
-   //    nextEl: '.swiper-button-next',
-   //    prevEl: '.swiper-button-prev',
-   // },
+   navigator: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
    pagination: {
       el: '.swiper-pagination',
       clickable: true,
