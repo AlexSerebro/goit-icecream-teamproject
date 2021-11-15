1fc123257a9f32c0b4d33cdc8e6d6f0863cff808
eijjeka <eijjeka@gmail.com>
2021-11-15 23:50:06 +0200

    add modal structure, styles and animate

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,21 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+
+(() => {
+  const refs = {
+
+    openModalBtn: document.querySelector('[data-modal-header-open]'),
+    closeModalBtn: document.querySelector('[data-modal-header-close]'),
+    modal: document.querySelector('[data-modal-header]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('active');
+  }
+})();
