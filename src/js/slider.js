b82cdb03801de12ec473a058d1675277f0f4ad43
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2021-11-16 21:35:10 +0200

    added flip effect

--- src/js/slider.js
+++ src/js/slider.js
@@ -7,4 +7,22 @@ new Swiper('.reviews__slider', {
       el: '.swiper-pagination',
       clickable: true,
    },
+   
+   grabCursor: true,
+   loop: true,
+   autoplay: {
+      deley: 1000,
+      disableOnInteraction: false,
+   },
+   speed: 800,
+
+   effect: 'flip',
+   flipEffect: {
+      slideShadows: false,
+      limitRotation: true,
+   },
+   
+   
+
+  
 });
